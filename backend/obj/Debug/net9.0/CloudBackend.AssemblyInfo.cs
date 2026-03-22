d68e83fab221290d23c490ba37fc334468c51e0b
adamusek-007 <adam.kuczabinski@gmail.com>
2026-03-22 12:45:35 +0000

    Add ef migrations

--- backend/obj/Debug/net9.0/CloudBackend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/CloudBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CloudBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35aa9e396c1311c25a9fc0d3b47a77a6ba6ad1c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7be691151a680997a156ab120fcc7dde31df2040")]
 [assembly: System.Reflection.AssemblyProductAttribute("CloudBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CloudBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
