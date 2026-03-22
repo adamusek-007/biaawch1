76ff8dc224f406faeb8149f0a50e7e7f4005bcd1
adamusek-007 <adam.kuczabinski@gmail.com>
2026-03-22 12:54:03 +0000

    dotnet ef database update

--- backend/obj/Debug/net9.0/CloudBackend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/CloudBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CloudBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7be691151a680997a156ab120fcc7dde31df2040")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+774821d8f56dbb17d6752e6bbd2951e9779ea4d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CloudBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CloudBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
